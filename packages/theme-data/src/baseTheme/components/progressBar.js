0e2ac3438d4d7e5ee708e7d81bc7b72bd99ca17a
Sean Durham <sean@sdurham.net>
2018-10-25 15:06:17 -0700

    feat: add theme data roles for progress bar

---
+++ packages/theme-data/src/baseTheme/components/progressBar.js
@@ -0,0 +1,16 @@
+import { COLOR, BORDER_RADIUS, LENGTH } from "../../consts/types";
+
+export default {
+  "progressBar.backgroundColor": {
+    type: COLOR
+  },
+  "progressBar.highlightColor": {
+    type: COLOR
+  },
+  "progressBar.borderRadius": {
+    type: BORDER_RADIUS
+  },
+  "progressBar.height": {
+    type: LENGTH
+  }
+};
