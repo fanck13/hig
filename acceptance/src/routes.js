9da1a5333f048a4cdda6af727cced3968ae8eca2
Sean Durham <sean@sdurham.net>
2018-12-19 12:01:03 -0800

    docs: adds acceptance page for Avatar

--- acceptance/src/routes.js
+++ acceptance/src/routes.js
@@ -1,9 +1,11 @@
+import AvatarPage from './pages/AvatarPage';
 import ButtonsPage from './pages/ButtonsPage';
 import FlyoutPage from './pages/FlyoutPage';
 import CheckboxPage from './pages/CheckboxPage';
 import InputPage from './pages/InputPage';
 
 export default [
+  { path: '/avatar/', component: AvatarPage, label: "Avatar" },
   { path: '/button/', component: ButtonsPage, label: "Button" },
   { path: '/flyout/', component: FlyoutPage, label: "Flyout" },
   { path: '/checkbox/', component: CheckboxPage, label: "Checkbox" },
